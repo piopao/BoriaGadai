c724c3d286dbc9bce54efb92eb21d93c1f1f8793
Vano <pc@Lenovo-PC>
2015-06-07 19:59:02 +0400

    minor update

--- SqlDatabase/db_tables.sql
+++ SqlDatabase/db_tables.sql
@@ -1,4 +1,4 @@
-DROP DATABBASE IF EXISTS db;
+DROP DATABASE IF EXISTS db;
 
 CREATE DATABASE db;
 USE db;
@@ -6,60 +6,75 @@ USE db;
 
 
 CREATE TABLE IF NOT EXISTS users (
-userid INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+	user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
 	username CHAR(64) NOT NULL ,
 	hashed_password CHAR(64) NOT NULL,
 	email_address VARCHAR(254) NOT NULL,
 	birthdate DATE,
 	gender CHAR(8),
 	avatar_filename VARCHAR(128),
-	info CHAR(512)
+	info TEXT
 );
-
---dir_names for default avatar pictures.
+CREATE TABLE IF NOT EXISTS quiz (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    quiz_name TEXT
+	
+	
+);
+#dir_names for default avatar pictures
 CREATE TABLE IF NOT EXISTS pictures (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
-	location NCHAR(128);
+	location NCHAR(128)
 );
 
 CREATE TABLE IF NOT EXISTS quiz_reviews (
-review_text TEXT CHARACTER SET utf8,
-review_rating INT,
-review_date DATETIME,
-FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-FOREIGN KEY (quiz_id) REFERENCES quiz(id) ON DELETE CASCADE
+	review_text TEXT CHARACTER SET utf8,
+	review_rating INT,
+	review_date DATETIME,
+	user_id INT,
+	quiz_id INT,
+	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+	FOREIGN KEY (quiz_id) REFERENCES quiz(id) ON DELETE CASCADE
 	
 );
 
 CREATE TABLE IF NOT EXISTS admin (
+	user_id INT,
 	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS banned_accounts (
+	user_id INT,
 	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS friend_list (
-	FOREIGN KEY (userA_id) REFERENCES users(user_id) ON DELETE CASCADE
+	userA_id INT,
+    userB_id INT,
+	FOREIGN KEY (userA_id) REFERENCES users(user_id) ON DELETE CASCADE,
 	FOREIGN KEY (userB_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS pending_friend_list (
-	FOREIGN KEY (userA_id) REFERENCES users(user_id) ON DELETE CASCADE
+	userA_id INT,
+    userB_id INT,
+	FOREIGN KEY (userA_id) REFERENCES users(user_id) ON DELETE CASCADE,
 	FOREIGN KEY (userB_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 
---quiz prediction history for users.
+#quiz prediction history for users.
 CREATE TABLE IF NOT EXISTS quiz_predictions (
 	prediction_text TEXT CHARACTER SET utf8,
 	prediction_date DATETIME,
+    user_id INT,
+    quiz_id INT,
 	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
 	FOREIGN KEY (quiz_id) REFERENCES quiz(id) ON DELETE CASCADE
 );
 
 
---taror cards designs.
+#taror cards designs.
 CREATE TABLE IF NOT EXISTS cards (
 	card_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
 	designA char(128),
@@ -70,6 +85,7 @@ CREATE TABLE IF NOT EXISTS cards (
 --
 CREATE TABLE IF NOT EXISTS fortune_card (
 	prediction_text TEXT CHARACTER SET utf8,
+    card_id INT,
 	FOREIGN KEY (card_id) REFERENCES cards(card_id) ON DELETE CASCADE
 	
 );
