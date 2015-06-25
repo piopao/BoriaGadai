f81bfd44ee23aaf36900887926bdc920adf87ef8
meripepanashvili <mpepa13@freeuni.edu.ge>
2015-06-26 03:22:51 +0400

    ს

--- src/lottery/LotteryServlet.java
+++ src/lottery/LotteryServlet.java
@@ -83,25 +83,3 @@ public class LotteryServlet extends HttpServlet {
 	}
 
 }
-/*
-function loadXMLDoc(numbers)
-{
-var xmlhttp;
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {	  
-	  document.getElementById("response").innerHTML = xmlhttp.responseText;
-    }
-}
-	xmlhttp.open("GET","LotteryHelper.jsp?"+numbers,true);
-	xmlhttp.send();	
-}*/
