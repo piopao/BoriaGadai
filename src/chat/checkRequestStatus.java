fadfc3323eff763c508ca9ad9f66010a9751a89c
Vano <itkem12@freeuni.edu.ge>
2015-06-28 18:26:03 +0400

    chat request thingies

---
+++ src/chat/checkRequestStatus.java
@@ -0,0 +1,51 @@
+package chat;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Fteller.db.managers.ChatManager;
+import authorization.User;
+
+/**
+ * Servlet implementation class checkRequestStatus
+ */
+public class checkRequestStatus extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public checkRequestStatus() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession sess = request.getSession();
+		User temp = (User) sess.getAttribute("user");
+		ServletContext context = getServletContext();
+		ChatManager chatManager = (ChatManager)context.getAttribute("ChatManager");
+		
+		int status = chatManager.checkRequestStatus(temp.getEmail());
+		PrintWriter out = response.getWriter();
+		out.print(status);
+	}
+
+}
---
+++ src/chat/deleteChatRequest.java
@@ -0,0 +1,50 @@
+package chat;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Fteller.db.managers.ChatManager;
+import authorization.User;
+
+/**
+ * Servlet implementation class deleteChatRequest
+ */
+public class deleteChatRequest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public deleteChatRequest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession sess = request.getSession();
+		User temp = (User) sess.getAttribute("user");
+		ServletContext context = getServletContext();
+		ChatManager chatManager = (ChatManager)context.getAttribute("ChatManager");
+		
+		chatManager.deleteChatRequest(temp.getEmail());
+		
+	}
+
+}
