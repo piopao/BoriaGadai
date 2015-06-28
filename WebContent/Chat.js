1b1d0ca9ea97bbab830a8fa538f6ed5daf700986
piopao <ekiknadze@gmail.com>
2015-06-29 00:29:09 +0400

    minor changes/chat window

--- WebContent/Chat.js
+++ WebContent/Chat.js
@@ -7,22 +7,23 @@ function myTimer() {
     document.getElementById("demo").innerHTML = t;
 }
 
-var ch = setInterval(function(){ checkMes() }, 4000);
+//var ch = setInterval(function(){ checkMes() }, 2000);
 
 function checkMes(){
 		 $.post("checkMessagesServlet",
 				 { },			        
 			      function(data,status){
-				   	var textfield = document.getElementById("message");
-				   	var textdiv = document.getElementById("textarea");
-				  	var paragraph = document.createElement("p");
-				  	var node = document.createTextNode(data + ": " + textfield.value);
-				   	paragraph.appendChild(node);
-				   	textdiv.appendChild(paragraph);
+					if(data!=null){
+					   	var textfield = document.getElementById("message");
+					   	var textdiv = document.getElementById("textarea");
+					  	var paragraph = document.createElement("p");
+					  	var node = document.createTextNode(data + ": " + textfield.value);
+					   	paragraph.appendChild(node);
+					   	textdiv.appendChild(paragraph);
+				   	}
 				   	return true;  			        	      	
 		 });	
 }
-
 
 
 $(document).keypress(function(e) {
@@ -34,7 +35,7 @@ $(document).keypress(function(e) {
 window.setInterval(function() {
 	  var elem = document.getElementById('textarea');
 	  elem.scrollTop = elem.scrollHeight;
-	}, 5000);
+	}, 1000);
 
 function sendMessage(){
 	 $.post("CurrentUserServlet",
@@ -46,6 +47,20 @@ function sendMessage(){
 		        	var node = document.createTextNode(data + ": " + textfield.value);
 		        	paragraph.appendChild(node);
 		        	textdiv.appendChild(paragraph);
+		        	document.getElementById("message").value = " ";
 		        	return true;       	
 	 });	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
