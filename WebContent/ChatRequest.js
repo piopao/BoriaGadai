5c21f396efad2001b82ab205a9c2f9359ee638ab
piopao <ekiknadze@gmail.com>
2015-06-28 18:50:00 +0400

    enterebi

--- WebContent/ChatRequest.js
+++ WebContent/ChatRequest.js
@@ -2,12 +2,18 @@
  * 
  */
 
-var myVar = setInterval(function(){ checkRequest() }, 1000);
+var myVar = setInterval(function(){ checkRequest() }, 8000);
 
 function checkRequest() {
 	 $.post("CheckRegistrationServlet",
 		        {},
 		        function(data,status){
+		        	if(data.length  == 0 ){
+		        		alert("length0");
+		        	}
+		        	else{
+		        		alert("length9");
+		        	}
 		        	
 		        });
 }
