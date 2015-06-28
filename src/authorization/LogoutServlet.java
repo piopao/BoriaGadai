d9738936cb74908fb650992ee302cd72ae11c434
ninucabas <nbasi13@freeuni.edu.ge>
2015-06-28 17:31:37 +0400

    useri contextidan amovagde

--- src/authorization/LogoutServlet.java
+++ src/authorization/LogoutServlet.java
@@ -3,6 +3,7 @@ package authorization;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -35,11 +36,17 @@ public class LogoutServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession sess = request.getSession();
+		ServletContext context = getServletContext();
+		User user = (User) sess.getAttribute("user");
 		String res = "";
 		if(sess.getAttribute("login") == "Gmail"){
 			res = "gmail";
 		} else if (sess.getAttribute("login") == "Facebook")
 			res = "facebook";
+		String email = user.getEmail();
+		if(context.getAttribute(email) != null) {
+		     context.removeAttribute(email);
+		}
 		sess.setAttribute("user", null);
 		sess.setAttribute("login", null);
 		response.setCharacterEncoding("UTF-8");
