6278e771b69060e51627648faf2673196b21b52f
piopao <ekiknadze@gmail.com>
2015-06-29 01:55:59 +0400

    accountmanager-is listenershi chavaswore

--- src/DBSettings/DBLogin.java
+++ src/DBSettings/DBLogin.java
@@ -3,5 +3,5 @@ package DBSettings;
 
 public class DBLogin {
 	public static final String USERNAME = "root";
-	public static final String PASSWORD = "14031403";//user your password
+	public static final String PASSWORD = "";//user your password
 }
