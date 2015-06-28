35031b97e457b7ee54b9010c10aca1005cb7f16e
ninucabas <nbasi13@freeuni.edu.ge>
2015-06-28 20:59:45 +0400

    egaa

---
+++ src/checkUser/CheckUserServlet.java
@@ -0,0 +1,58 @@
+package checkUser;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import authorization.User;
+
+/**
+ * Servlet implementation class CheckUserServlet
+ */
+public class CheckUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CheckUserServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		HttpSession sess = request.getSession();
+		String user = "";
+		System.out.println("olaaaaaaaaa");
+		User currentUser = (User) sess.getAttribute("user");
+		if (currentUser == null) {
+			user = "სტუმარი";
+		} else {
+			user = (String) currentUser.getEmail();
+		}
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print(user);
+		out.close();
+	}
+
+}
