2d612e0005086e509a966105840d95a7316a5937
piopao <ekiknadze@gmail.com>
2015-06-29 18:58:08 +0400

    დსა

--- src/DBSettings/DBLogin.java
+++ src/DBSettings/DBLogin.java
@@ -3,5 +3,5 @@ package DBSettings;
 
 public class DBLogin {
 	public static final String USERNAME = "root";
-	public static final String PASSWORD = "1234";//user your password
+	public static final String PASSWORD = "";//user your password
 }
