a9ce315f57fa4f5f9bd88f9eb729751108adab5c
piopao <ekiknadze@gmail.com>
2015-06-29 22:39:24 +0400

    chat card flips

--- WebContent/Chat.js
+++ WebContent/Chat.js
@@ -7,17 +7,18 @@ function myTimer() {
     document.getElementById("demo").innerHTML = t;
 }
 
-var ch = setInterval(function(){ checkMes() }, 2000);
+//var ch = setInterval(function(){ checkMes() }, 2000);
 
 function checkMes(){
 		 $.post("checkMessagesServlet",
 				 { },			        
 			      function(data,status){
-					if(data!=null){
+					 alert("dsa");
+					if(data.length != 0){						
 					   	var textfield = document.getElementById("message");
 					   	var textdiv = document.getElementById("textarea");
 					  	var paragraph = document.createElement("p");
-					  	var node = document.createTextNode(data + ": " + textfield.value);
+					  	var node = document.createTextNode(data);
 					   	paragraph.appendChild(node);
 					   	textdiv.appendChild(paragraph);
 				   	}
@@ -56,8 +57,46 @@ function sendMessage(){
 
 
 
+function flip1(_src){
+	var tokenedIntoPair =  _src.split("/");
+	var length = tokenedIntoPair.length;
+	var backsrc = tokenedIntoPair[length-1];
+	if(backsrc == "back2.jpg"){
+		document.getElementById("1").src="./Images/Decks/tarot deck chat/2.jpg";
+	}
+	return true;
+}
+
+function flip2(_src){
+	var tokenedIntoPair =  _src.split("/");
+	var length = tokenedIntoPair.length;
+	var backsrc = tokenedIntoPair[length-1];
+	if(backsrc == "back2.jpg"){
+		document.getElementById("2").src="./Images/Decks/tarot deck chat/32.jpg";
+	}
+	return true;
+}
+
+function flip3(_src){
+	var tokenedIntoPair =  _src.split("/");
+	var length = tokenedIntoPair.length;
+	var backsrc = tokenedIntoPair[length-1];
+	if(backsrc == "back2.jpg"){
+		document.getElementById("3").src="./Images/Decks/tarot deck chat/4.jpg";
+	}
+	return true;
+}
 
 
+function flip4(_src){
+	var tokenedIntoPair =  _src.split("/");
+	var length = tokenedIntoPair.length;
+	var backsrc = tokenedIntoPair[length-1];
+	if(backsrc == "back2.jpg"){
+		document.getElementById("4").src="./Images/Decks/tarot deck chat/65.jpg";
+	}
+	return true;
+}
 
 
 
