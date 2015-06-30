e17aa8f54772e103be4a29d52f08c40de8eeb940
Vano <itkem12@freeuni.edu.ge>
2015-07-01 00:08:45 +0400

    a

--- SqlDatabase/db_tables.sql
+++ SqlDatabase/db_tables.sql
@@ -25,10 +25,10 @@ CREATE TABLE IF NOT EXISTS image_table (
 
 CREATE TABLE IF NOT EXISTS game_table (
 	game_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
-    game_name VARCHAR(32)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL UNIQUE ,
+    game_name VARCHAR(64)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL UNIQUE ,
 	game_description TEXT  CHARACTER SET utf8 COLLATE utf8_unicode_ci,
-    image_name varchar(32) not null unique,
-    game_url VARCHAR(32),
+    image_name varchar(64) not null unique,
+    game_url VARCHAR(64),
     FOREIGN KEY (image_name) REFERENCES image_table(image_dir) ON DELETE CASCADE
 );
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS quiz_reviews (
 	review_text TEXT  CHARACTER SET utf8 COLLATE utf8_unicode_ci,
 	review_rating INT,
 	review_date VARCHAR(64),
-    game_name varchar(32)  CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+    game_name varchar(64)  CHARACTER SET utf8 COLLATE utf8_unicode_ci,
 	user_email VARCHAR(64),
 	FOREIGN KEY (game_name) REFERENCES game_table(game_name) ON DELETE CASCADE,
 	FOREIGN KEY (user_email) REFERENCES users(email_address) ON DELETE CASCADE
@@ -73,13 +73,7 @@ CREATE TABLE IF NOT EXISTS pending_friend_list (
 
 
 
-#tarot cards designs.
-CREATE TABLE IF NOT EXISTS cards (
-	card_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
-	designA char(128),
-	designB char(128),
-	designC char(128)
-);
+
 
 --
 CREATE TABLE IF NOT EXISTS fortune_card (
@@ -94,7 +88,7 @@ CREATE TABLE IF NOT EXISTS fortune_card (
 
 CREATE TABLE IF NOT EXISTS weather_table (
 	weather_id INT PRIMARY KEY UNIQUE,
-    weather_text VARCHAR(128)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL UNIQUE,
+    weather_text TEXT  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL UNIQUE,
 	image_dir VARCHAR(64)
 );
 
@@ -103,7 +97,7 @@ CREATE TABLE IF NOT EXISTS weather_table (
 
 CREATE TABLE IF NOT EXISTS weather_history (
 	user_email VARCHAR(64),
-    text_weather VARCHAR(256)  CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+    text_weather TEXT  CHARACTER SET utf8 COLLATE utf8_unicode_ci,
 	save_date VARCHAR(64),
 	FOREIGN KEY (user_email) REFERENCES users(email_address) ON DELETE CASCADE
 	
@@ -139,7 +133,7 @@ CREATE TABLE IF NOT EXISTS new_chat_messages(
 
 CREATE TABLE IF NOT EXISTS fortune_texts(
 	text_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
-    fortune_text VARCHAR(128)
+    fortune_text TEXT
 );
 
 
