ea2413037aadcf447472ed5d2d358dd81e0f5163
piopao <ekiknadze@gmail.com>
2015-07-01 00:16:32 +0400

    mysessionlistener

---
+++ src/listener/MySessionListener.java
@@ -0,0 +1,39 @@
+package listener;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import authorization.User;
+
+/**
+ * Application Lifecycle Listener implementation class MySessionListener
+ *
+ */
+public class MySessionListener implements HttpSessionListener {
+
+    /**
+     * Default constructor. 
+     */
+    public MySessionListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent arg0)  { 
+    	HttpSession session = arg0.getSession();
+    	User usr = (User)session.getAttribute("user");
+    	String email = usr.getEmail();
+    	session.getServletContext().removeAttribute(email);
+    }
+	
+}
